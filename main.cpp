b39d06eda201723d89f031af0b15e871ca2fb80b
Alumno <jakii_paz.07@hotmail.es>
2015-05-02 14:28:41 -0600

    mi ejercicio

---
+++ main.cpp
@@ -0,0 +1,81 @@
+#include "Evaluador.h"
+#include <iostream>
+using namespace std;
+
+//Devuelve la suma de a (dado) y b (dado)
+int sumar(int a, int b)
+{
+    return -1;
+}
+
+//Devuelve la resta de a (dado) y b (dado)
+int restar(int a, int b)
+{
+    return -1;
+}
+
+//Devuelve la multiplicacion de a (dado) y b (dado)
+int multiplicar(int a, int b)
+{
+    return -1;
+}
+
+//Devuelve la division de a (dado) y b (dado)
+int dividir(int a, int b)
+{
+    return -1;
+}
+
+//Devuelve el numero mayor entre a (dado) y b (dado)
+int getMayor(int a, int b)
+{
+    return -1;
+}
+
+//Devuelve el numero mayor entre a (dado) y b (dado)
+int getMenor(int a, int b)
+{
+    return -1;
+}
+
+//Devuelve el numero mayor entre a (dado), b (dado) y c (dado)
+int getMayor(int a, int b, int c)
+{
+    return -1;
+}
+
+//Establece el valor (dado) en el arreglo (dado) en el indice posicion (dado)
+void setValor(int arreglo[], int valor, int posicion)
+{
+}
+
+//Obtiene y devuelve el valor del arreglo (dado) en el indice posicion (dado)
+int getValor(int arreglo[], int posicion)
+{
+    return -1;
+}
+
+//Devuelve el numero mayor del arreglo (dado) que contiene tamano (dado) elementos
+int getMayor(int arreglo[], int tamano)
+{
+    return -1;
+}
+
+//Devuelve el numero menor del arreglo (dado) que contiene tamano (dado) elementos
+int getMenor(int arreglo[], int tamano)
+{
+    return -1;
+}
+
+//Devuelve el promedio de los numeros del arreglo (dado) que contiene tamano (dado) elementos
+int getPromedio(int arreglo[], int tamano)
+{
+    return -1;
+}
+
+int main ()
+{
+    //Funcion evaluadora
+    evaluar();
+    return 0;
+}
